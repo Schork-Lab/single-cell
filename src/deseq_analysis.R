2a165206a690f25405199078e46557b38d55fbd2
Kunal Bhutani <kunalbhutani@gmail.com>
2015-01-23 10:51:32 -0800

    Updated with PCA. It was done using the 4192 highly expressed genes in the control sample. These genes were subselected in the htseq-count output, and then fed through DESeq. The DESeq adjusted values were then used to calculate the PCA.

--- src/deseq_analysis.R
+++ src/deseq_analysis.R
@@ -1,17 +1,33 @@
 library("DESeq2")
-library("DESeq")
-countData = read.csv('../../data/htseq_counts.R-ready.csv', sep=',', row.names=1)
+#library("DESeq")
+
+#Load in count data
+countData = read.csv('htseq_counts.high.R-ready.csv',
+												 sep=',', row.names=1)
+#countData = read.csv('htseq_counts.all.R-ready.csv', sep=',', row.names=1)												 
 samples = sort(colnames(countData))
 sample_types = c("Neuronal","Neuronal","Neuronal","Neuronal","Neuronal","Neuronal","Non-neuronal","Non-neuronal","Non-neuronal","Non-neuronal","Total RNA","Total RNA", "Total RNA", "Total RNA")
 colData = data.frame(samples, sample_types, row.names=1)
 dds <- DESeqDataSetFromMatrix(countData = countData,
 colData = colData,
 design = ~ sample_types)
+
+
 dds <- DESeq(dds)
 res <- results(dds)
-resMLE <- results(dds, addMLE=TRUE, contrast=c("sample_types", "Neuronal","Non-neuronal"))
+resMLE <- results(dds, addMLE=TRUE, 
+										contrast=c("sample_types", "Neuronal","Non-neuronal"))
+
+#Write to CSV
+write.csv(as.data.frame(resMLE), file="differential_high_expression.csv")
+
 rld <- rlog(dds)
-DESeq2::plotPCA(rld, intgroup=c("sample_types"))
+rlogMat <- assay(rld)
+
+write.csv(as.data.frame(rlogMat), file="deseq_adjusted_high_expression.csv")
+
+
+pca <- DESeq2::plotPCA(rld, intgroup=c("sample_types"))
 distsRL <- dist(t(assay(rld)))
 mat <- as.matrix(distsRL)
 rownames(mat) <- colnames(mat) <- with(colData(dds),
